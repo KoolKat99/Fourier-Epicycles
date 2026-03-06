6807e00f7c9cb82fa1aee274c028e5bce563ba3f
Ian <40438381+KoolKat99@users.noreply.github.com>
2026-03-05 21:15:52 -0800

    Delete _deps directory

--- _deps/kissfft-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ian/VSCODE/C++/Fourier Picture/build/_deps/kissfft-src
-# Build directory: /home/ian/VSCODE/C++/Fourier Picture/build/_deps/kissfft-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tools")
-subdirs("test")
